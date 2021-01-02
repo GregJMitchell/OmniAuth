a1b38ec38e364be962aecd656d811239b1433fa2
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-02 14:48:28 -0700

    Remove Pry

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,5 @@ class DashboardController < ApplicationController
   def show
     repos = RepoFacade.repos(current_user.token)
     @repos = Repo.sort_by_private(repos)
-    require 'pry'; binding.pry
   end
 end
